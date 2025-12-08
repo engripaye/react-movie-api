10e57ee2176550a2338a8c87f26f40677d979616
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2025-12-08 14:47:12 +0100

    commit latest development by ENGR. IPAYE

--- movie-frontend/src/App.jsx
+++ movie-frontend/src/App.jsx
@@ -3,19 +3,12 @@ import './App.css'
 function App() {
 
   return (
-      <>
+      <></>
 
-        <Text display="hello boy" />
-        <Text display="My name is "/>
-  </>
-  )
-}
-
-function Text({display}) {
-  return (<div>
-    <p>{display}</p>
-  </div>
   );
+
+
+
 }
 
 export default App
