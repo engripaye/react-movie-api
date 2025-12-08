0459a3c9ded0273aa42e23458147d05ca4510dad
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2025-12-08 20:09:22 +0100

    commit latest development by ENGR. IPAYE

---
+++ movie-frontend/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import MovieCard from "../component/MovieCard.jsx";
+
+function Home() {
+
+    const movies = [
+        {id: 1, title: "John Wick", release_date: "2020"},
+        {id: 2, title: "Terminator", release_date: "1998"},
+        {id: 3, title: "Matrix", release_date: "1999"},
+
+    ]
+
+
+    return <div className="Home">
+        <div className="movies-grid">
+            {movies.map(movie =>
+                <MovieCard movie={movie} key={movie.id}/> )}
+        </div>
+    </div>
+}
+
+export default Home
