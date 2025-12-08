d6ef60e1875756419cd0bb6782f719b7e2ad32cb
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2025-12-08 14:58:34 +0100

    commit latest development by ENGR. IPAYE

--- movie-frontend/src/component/MovieCard.jsx
+++ movie-frontend/src/component/MovieCard.jsx
@@ -1,4 +1,19 @@
 
-function MovieCard(){
+function MovieCard({movie}){
 
+    function onFavoriteClick(){
+        alert("click")
+    }
+
+    return <div className="movie-card">
+        <div className="movie-poster">
+            <img src={movie.url} alt={movie.title} />
+            <div className="movie-overlay">
+                <button className="favorite-btn" onClick={onFavoriteClick}>
+                    🤍
+                </button>
+            </div>
+        </div>
+
+    </div>
 }
