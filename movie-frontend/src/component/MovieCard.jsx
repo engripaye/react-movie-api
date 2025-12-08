bd73e93042d7f4ed2e4d9cdfa6244608341d2d7f
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2025-12-08 15:03:25 +0100

    commit latest development by ENGR. IPAYE

--- movie-frontend/src/component/MovieCard.jsx
+++ movie-frontend/src/component/MovieCard.jsx
@@ -10,10 +10,15 @@ function MovieCard({movie}){
             <img src={movie.url} alt={movie.title} />
             <div className="movie-overlay">
                 <button className="favorite-btn" onClick={onFavoriteClick}>
-                    🤍🤍
+                    🤍
                 </button>
             </div>
         </div>
-
+    <div className="movie-info">
+        <h3>{movie.title}</h3>
+        <p>{movie.release_date}</p>
+    </div>
     </div>
 }
+
+export default MovieCard;
