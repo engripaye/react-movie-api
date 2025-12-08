3067943ad0ebf209c8233961ad5700c892141f14
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2025-12-08 15:22:32 +0100

    commit latest development by ENGR. IPAYE

--- movie-frontend/src/App.jsx
+++ movie-frontend/src/App.jsx
@@ -2,12 +2,16 @@ import './App.css'
 import MovieCard from "./component/MovieCard.jsx";
 
 function App() {
+  const movieNumber = 1;
 
   return (
       <>
-      <MovieCard movie={{title: "Tim's film", release_date: "2024"}}/>
-      </>
+        {movieNumber === 1 ? (<MovieCard movie={{title: "Tim's film", release_date: "2024"}}/>)
+            : (
+            <MovieCard movie={{title: "Joe's film", release_date: "2020"}}/>
+          )}
 
+  </>
   );
 
 
