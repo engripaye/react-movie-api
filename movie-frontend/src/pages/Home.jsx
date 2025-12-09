8780dbc65d89dc4001e4cd43c5e12675fffc2c23
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2025-12-09 14:14:19 +0100

    commit latest development by ENGR. IPAYE

--- movie-frontend/src/pages/Home.jsx
+++ movie-frontend/src/pages/Home.jsx
@@ -7,12 +7,21 @@ function Home() {
         {id: 2, title: "Terminator", release_date: "1998"},
         {id: 3, title: "Matrix", release_date: "1999"},
 
-    ]
+    ];
+
+    const handleSearch = () => {
+
+    }
 
 
     return <div className="Home">
 
-        <form></form>
+        <form onSubmit={handleSearch} className="search-form">
+            <input type="test"
+                   placeholder="search for movies"
+                   className="search-input"/>
+            <button type="submit" className="search-button">Search</button>
+        </form>
         <div className="movies-grid">
             {movies.map(movie =>
                 <MovieCard movie={movie} key={movie.id}/>)}
