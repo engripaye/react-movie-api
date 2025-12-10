b9ee786f49a199145da2228844e7b031a11c3bf1
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2025-12-10 09:36:37 +0100

    commit latest development by ENGR. IPAYE

---
+++ movie-frontend/src/pages/Favorite.jsx
@@ -0,0 +1,8 @@
+function Favorite() {
+    return <div className="favorite-empty">
+        <h2> No Favorite Movies Yet</h2>
+        <P>Start adding movies to your favorites and they will appear here</P>
+    </div>
+}
+
+export default Favorite
