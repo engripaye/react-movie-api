94f7df819bdefe5b997b48d48985247600cb54b0
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2025-12-10 10:22:23 +0100

    commit latest development by ENGR. IPAYE

--- movie-frontend/src/App.jsx
+++ movie-frontend/src/App.jsx
@@ -1,14 +1,24 @@
 import './App.css'
 import Home from "./pages/Home.jsx";
+import {Route, Routes} from "react-router-dom";
+import Favorites from "./pages/Favorites.jsx";
 
 
 function App() {
 
 
   return (
-      <>
-        <Home />
-  </>
+      <main className="main-content">
+
+          <Routes>
+              <Route path="/" element={<Home />}/>
+              <Route path="/favorites" element={<Favorites />}/>
+
+          </Routes>
+      </main>
+
+
+
   );
 
 
