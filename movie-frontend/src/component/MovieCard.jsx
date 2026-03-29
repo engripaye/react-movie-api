579cb3507fb0b4c478aaacfd4c846a66901fc6ee
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-03-29 10:39:26 +0100

    commit latest development by ENGR. IPAYE

--- movie-frontend/src/component/MovieCard.jsx
+++ movie-frontend/src/component/MovieCard.jsx
@@ -13,6 +13,7 @@ function MovieCard({movie}){
 
     //
 
+
     return <div className="movie-card">
         <div className="movie-poster">
             <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
