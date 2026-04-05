6dfeeb9480c4b7ef63c3176a35275453d09519dd
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-04-05 16:51:11 +0100

    commit latest development by ENGR. IPAYE

--- movie-frontend/src/component/Navbar.jsx
+++ movie-frontend/src/component/Navbar.jsx
@@ -3,7 +3,7 @@ import "../css/Navbar.css"
 function Navbar() {
     return <nav className="navbar">
         <div className="navbar-brand">
-            <Link to="/">Movie App</Link>
+            <Link to="/">Engr. Ipaye's MovieApp</Link>
 
         </div>
         <div className="navbar-links">
