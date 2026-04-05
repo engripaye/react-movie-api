ad0d4748e64aa0d6e5e8e86a1065470e9808c790
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-04-05 12:28:42 +0100

    commit latest development by ENGR. IPAYE

--- movie-frontend/vite.config.js
+++ movie-frontend/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: './', // relative paths for assets
+  // remove `root` — let Vite detect automatically
 });
