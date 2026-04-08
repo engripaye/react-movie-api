26c09adde52bece87cb5a1c7e5fb6cfb8e14f9de
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-04-08 09:47:10 +0100

    commit latest development by ENGR. IPAYE

--- movie-frontend/src/services/api.js
+++ movie-frontend/src/services/api.js
@@ -2,24 +2,10 @@ const API_KEY = "2a523adf071b2222307a4fb87ed980c4"
 const BASE_URL = "https://api.themoviedb.org/3"
 
 
-const fetchMultiplePages = async (url, pages = 2) => {
-    let results = [];
-
-    for (let i = 1; i <= pages; i++) {
-        const res = await fetch(`${url}&page=${i}`);
-        const data = await res.json();
-        results = [...results, ...(data.results || [])];
-    }
-
-    return results;
-};
-
-
 export const getPopularMovies = async () => {
-    return await fetchMultiplePages(
-        `${BASE_URL}/movie/popular?api_key=${API_KEY}`,
-        2
-    );
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
+    const data = await response.json()
+    return data.results
 };
 
 export const searchMovies = async (query) => {
@@ -31,17 +17,15 @@ export const searchMovies = async (query) => {
 };
 
 export const getNowPlayingMovies = async () => {
-    return await fetchMultiplePages(
-        `${BASE_URL}/movie/now_playing?api_key=${API_KEY}`,
-        2
-    );
+    const response = await fetch(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}`)
+    const data = await response.json()
+    return data.results
 };
 
 export const getTopRatedMovies = async () => {
-    return await fetchMultiplePages(
-        `${BASE_URL}/movie/top_rated?api_key=${API_KEY}`,
-        2
-    );
+    const response = await fetch(`${BASE_URL}/movie/top_rated?api_key=${API_KEY}`)
+    const data = await response.json()
+    return data.results
 };
 
 export const getMovieTrailer = async (id, type = "movie") => {
@@ -58,29 +42,34 @@ export const getMovieTrailer = async (id, type = "movie") => {
 };
 
 export const getPopularTV = async () => {
-    return await fetchMultiplePages(
-        `${BASE_URL}/tv/popular?api_key=${API_KEY}`,
-        2
+    const res = await fetch(
+        `${BASE_URL}/tv/popular?api_key=${API_KEY}`
     );
+    const data = await res.json();
+    return data.results;
 };
 
 export const getKoreanTV = async () => {
-    return await fetchMultiplePages(
-        `${BASE_URL}/discover/tv?api_key=${API_KEY}&with_original_language=ko&sort_by=popularity.desc`,
-        2
+    const res = await fetch(
+        `${BASE_URL}/discover/tv?api_key=${API_KEY}&with_original_language=ko&sort_by=popularity.desc`
     );
+    const data = await res.json();
+    return data.results;
 };
+
 export const getLatestKoreanTV = async () => {
-    return await fetchMultiplePages(
-        `${BASE_URL}/discover/tv?api_key=${API_KEY}&with_original_language=ko&sort_by=first_air_date.desc`,
-        2
+    const res = await fetch(
+        `${BASE_URL}/discover/tv?api_key=${API_KEY}&with_original_language=ko&sort_by=first_air_date.desc`
     );
+    const data = await res.json();
+    return data.results;
 };
 
 // Get popular Telenovelas (Spanish/Latin dramas)
 export const getTelenovelas = async () => {
-    return await fetchMultiplePages(
-        `${BASE_URL}/discover/tv?api_key=${API_KEY}&with_original_language=es&sort_by=popularity.desc`,
-        2
+    const res = await fetch(
+        `${BASE_URL}/discover/tv?api_key=${API_KEY}&with_original_language=es&sort_by=popularity.desc`
     );
+    const data = await res.json();
+    return data.results;
 };
